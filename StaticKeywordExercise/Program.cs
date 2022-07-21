f056e63df60b70221031111a76bc9fc57103e4f3
Damion Russell <domino.russell@gmail.com>
2022-07-21 11:03:53 -0400

    initial commit

---
+++ StaticKeywordExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticKeywordExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(32));
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(80));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StaticKeywordExercise/TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticKeywordExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double x)
+        {
+            return (x - 32) * 0.5556;
+        }
+        public static double CelsiusToFahrenheit(double x)
+        {
+            return (x * 1.8) + 32;
+        }
+    }
+}
